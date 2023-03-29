78739f50f52e9337d2d0e725f70e609de4140b51
SkyRavenS1lver <101544440+SkyRavenS1lver@users.noreply.github.com>
2023-03-29 12:55:47 +0700

    initial

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -9,16 +9,48 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnSubmit ;
+    EditText harga ;
+    TextView result ;
+    TextView currency ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSubmit = findViewById(R.id.submit);
+        harga = findViewById(R.id.inputHarga);
+        result = findViewById(R.id.result);
+        currency = findViewById(R.id.currency);
+        Locale currentLocale = getResources().getConfiguration().locale;
+        Currency currentCurrency = Currency.getInstance(currentLocale);
+        currency.setText("("+currentCurrency.getDisplayName()+")");
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double hasilnya = Double.parseDouble(harga.getText().toString())*100;
 
+                NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(currentLocale);
+                String results = currencyFormatter.format(hasilnya);
+                result.setText(results);
+            }
+        });
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
                 showHelp();
             }
         });
+        Date date = new Date();
+        long expirationDate = date.getTime() + TimeUnit.DAYS.toMillis(3);
+        date.setTime(expirationDate);
+        String formatDate = DateFormat.getDateInstance().format(date);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
     }
 
     /**
@@ -63,6 +101,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.action_help) {
             Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_language) {
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
             startActivity(intent);
             return true;
         }
